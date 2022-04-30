ed8aa14cf84b6dc88901ad513bf8a8724c25f444
rcsfc <102752666+rcsfc@users.noreply.github.com>
2022-04-30 01:47:00 -0400

    Update launch_instance.sh

--- launch_instance.sh
+++ launch_instance.sh
@@ -10,7 +10,6 @@ read project_name
 # Run our Bash commands
 if [ ! -d ~/terraform/$project_name ]; then
    mkdir -p ~/terraform/$project_name
-   mkdir -p ~/terraform/state_backups/$project_name
    cd  ~/terraform/$project_name
    git clone https://github.com/rcsfc/terraform-aws-vpc-ec2-s3-automated.git .
    find . -type f -print0 | xargs -0 sed -i "s/CHANGEME/$project_name/g"
@@ -24,6 +23,7 @@ fi
 # Copy our state files to the backup folder so we can retrieve them later
 # This isn't recommended for production use and is here more to spark the idea that you should backup your state files (but in a secure way)
 if [ ! -d ~/terraform/state_backups/$project_name ]; then
+   mkdir -p ~/terraform/state_backups/$project_name
    find . -name "*.tfstate" -type f -exec cp {} ~/terraform/state_backups/$project_name/ \;
 else
    echo "Project backup directory already exists. Did you already create this project?"
