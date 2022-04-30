ede021461a84c2e8d4ee9ec801197dab36c5cd30
rcsfc <102752666+rcsfc@users.noreply.github.com>
2022-04-30 01:14:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc-ec2-s3-automated" {
-  source = "github.com/rcsfc/terraform-aws-vpc-ec2-s3//modules/vpc-ec2-s3-automated"
+  source = "github.com/rcsfc/terraform-aws-vpc-ec2-s3-automated//modules/vpc-ec2-s3-automated"
   instance_type = var.instance_type
   availability_zone = var.availability_zone
   CHANGEME_key_path = var.CHANGEME_key_path
